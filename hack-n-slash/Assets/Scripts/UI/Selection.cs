9da09abfaf0d58f16112a96141ca2209862e466c
DusaPraharsh <dusapraharsh123@gmail.com>
2025-05-09 18:02:59 +0530

    UI Selection

--- hack-n-slash/Assets/Scripts/UI/Selection.cs
+++ hack-n-slash/Assets/Scripts/UI/Selection.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class Selection : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public RectTransform[] options;
+    private RectTransform rect;
+    private int currentPosition;
+    public AudioClip selectionSound;
+    public AudioClip interactSound;
+
+    private void Awake()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangePosition(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ChangePosition(1);
+        }
+    }
+
+    private void ChangePosition(int _change)
+    {
+        currentPosition += _change;
+
+        if (_change != 0)
+            SoundManager.instance.PlaySound(selectionSound);
+
+        if (currentPosition < 0)
+        {
+            currentPosition = options.Length - 1;
+        }
+        else if (currentPosition > options.Length - 1)
+        {
+            currentPosition = 0;
+        }
+
+        rect.position = new Vector3(rect.position.x, options[currentPosition].position.y, 0);
     }
 }
