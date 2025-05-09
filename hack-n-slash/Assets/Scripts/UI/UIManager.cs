d9bae559572cb161e7e530c9831396c3724c3542
DusaPraharsh <dusapraharsh123@gmail.com>
2025-05-09 20:58:51 +0530

    pause menu

--- hack-n-slash/Assets/Scripts/UI/UIManager.cs
+++ hack-n-slash/Assets/Scripts/UI/UIManager.cs
@@ -6,10 +6,26 @@ public class UIManager : MonoBehaviour
     public GameObject gameOverScreen;
     public AudioClip gameOverSound;
 
+    public GameObject pauseScreen;
+
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+        }
+    }
+
+    #region Game Over
 
     public void GameOver()
     {
@@ -30,5 +46,34 @@ public class UIManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
+    #endregion
+
+    #region Pause
+
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        if (status)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void Volume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f);
+    }
+
+    public void Music()
+    {
+        
+    }
+
+    #endregion
 }
