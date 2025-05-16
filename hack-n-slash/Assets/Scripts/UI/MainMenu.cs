83551f75720671ce00438c2e0be1fee9dd68e1d5
DusaPraharsh <dusapraharsh123@gmail.com>
2025-05-16 22:05:11 +0530

    final push

--- hack-n-slash/Assets/Scripts/UI/MainMenu.cs
+++ hack-n-slash/Assets/Scripts/UI/MainMenu.cs
@@ -3,26 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject menuScreen;
-
-    private void Awake()
-    {
-        menuScreen.SetActive(true);
-    }
-
     public void Play()
     {
         SceneManager.LoadScene(1);
-    }
-
-    public void Volume()
-    {
-        SoundManager.instance.ChangeSoundVolume(0.2f);
-    }
-
-    public void Music()
-    {
-        SoundManager.instance.ChangeMusicVolume(0.2f);
     }
 
     public void Quit()
